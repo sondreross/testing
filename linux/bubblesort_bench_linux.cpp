7ba2dd0e767d2ba9ecffd0abcec70bc65d7558ad
Sondre <sondre.rosslund@gmail.com>
2025-10-20 11:53:31 +0200

    change to 30

--- linux/bubblesort_bench_linux.cpp
+++ linux/bubblesort_bench_linux.cpp
@@ -110,7 +110,7 @@ int main() {
     printf("array_size,cpu_cycles,cycles_start,cycles_end,time_ns,time_ms,pkg_joules,pkg_mJ,dram_joules,dram_mJ,total_joules,total_mJ\n");
     
     const size_t sizes[] = {2000, 50000, 100000};
-    const int repetitions = 50;
+    const int repetitions = 30;
     
     for (size_t size : sizes) {
         for (int rep = 0; rep < repetitions; rep++) {
