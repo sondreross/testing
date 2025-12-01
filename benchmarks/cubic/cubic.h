2d0627b95722b3debf8e10d2899e8f2d0fce85bc
Sondre <sondre.rosslund@gmail.com>
2025-12-01 12:44:25 +0100

    include header

--- benchmarks/cubic/cubic.h
+++ benchmarks/cubic/cubic.h
@@ -6,9 +6,4 @@ void initialise_benchmark_cubic(void);
 int verify_benchmark_cubic(int r);
 char* get_benchmark_name_cubic(void);
 
-int cubic2(void);
-void initialise_benchmark_cubic2(void);
-int verify_benchmark_cubic2(int r);
-char* get_benchmark_name_cubic2(void);
-
 #endif /* CUBIC_H */
